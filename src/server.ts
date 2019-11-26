cad00be23567dff0c9c42a1ff8b56c599d5bdf2b
New-AH <thomas.george@edu.ece.fr>
2019-11-26 12:39:13 +0100

    Cours

---
+++ src/server.ts
@@ -0,0 +1,55 @@
+import express = require('express')
+import { MetricsHandler } from './metrics'
+import path = require('path')
+import bodyparser = require('body-parser')
+
+const app = express()
+const port: string = process.env.PORT || '8082'
+app.use(express.static(path.join(__dirname, '/../public')))
+
+app.set('views', __dirname + "/../views")
+app.set('view engine', 'ejs');
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded())
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.get('/hello/:name', (req: any, res: any) => {
+  res.render('hello.ejs', {name: req.params.name})
+})
+
+const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+
+app.post('/metrics/:id', (req: any, res: any) => {
+  dbMet.save(req.params.id, req.body, (err: Error | null) => {
+    if (err) throw err
+    res.status(200).send('ok')
+  })
+})
+
+app.get('/metrics/', (req: any, res: any) => {
+  dbMet.getAll(
+    (err: Error | null , result: any) => {
+    if (err) throw err
+    res.status(200).send(result)
+  })
+})
+
+app.get('/metrics/one', (req: any, res: any) => {
+  dbMet.get1(
+    (err: Error | null , result: any) => {
+    if (err) throw err
+    res.status(200).send(result)
+  })
+})
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Server is running on http://localhost:${port}`)
+})
